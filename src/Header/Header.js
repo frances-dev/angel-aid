7cd3d3520054e8e6976ee687cb76467eb2a5ef53
yusuf0174 <shahriarfrances@gmail.com>
2021-10-20 19:53:11 +0600

    FInal Push

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -25,13 +25,13 @@ const Header = () => {
                             <Link to="/about" className="my-hover text-white text-decoration-none">About Us</Link>
                             <Link to="/book-an-appointment" className="my-hover text-white text-decoration-none">Book an Appointment</Link>
                         </Nav>
-                        <Nav>
+                        <Nav className='my-nav'>
                             {
                                 user.name ?
-                                    <div className='d-flex align-items-center'>
+                                    <div className='nav-div d-flex align-items-center'>
                                         <img src='http://sanjaymotels.com/wp-content/uploads/2019/01/testimony.png' alt="" width='35px' height='35px' />
-                                        <h3 className='text-light small mt-2 me-3 ms-3'>{user.name}</h3>
-                                        <button className="btn-warning me-3 ps-3 pe-3 pt-1 pb-1 border-0 rounded-2" onClick={logOut}>Sign Out</button>
+                                        <h3 className='text-light small mt-2 pe-3 ms-3'>{user.name}</h3>
+                                        <button className="btn-warning ps-3 pe-3 pt-1 pb-1 border-0 rounded-2" onClick={logOut}>Sign Out</button>
                                     </div> :
                                     <Link to="/sign-up" className="my-hover text-white text-decoration-none">
                                         {element2} Register
